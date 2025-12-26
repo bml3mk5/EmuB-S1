bfa6b54e948ffe4817c8192a87ea6465087fa99d
Sasaji <sasaji@s-sasaji.ddo.jp>
2025-12-27 00:54:29 +0900

    Version 0.9.2

--- src/source/Qt/qt_trans.bat
+++ src/source/Qt/qt_trans.bat
@@ -3,5 +3,6 @@ rem @echo off
 set path=%path%;D:\Qt\5.12.4\msvc2017_64\bin
 set path=%path%;D:\Qt\Tools\QtCreator\bin
 
+lconvert ..\locale\ja\LC_MESSAGES\mbs1.po -o ..\locale\ja\mbs1_ja.ts
 lupdate -tr-function-alias QT_TR_NOOP+=_TX mbs1_qt.pro
 lrelease mbs1_qt.pro
